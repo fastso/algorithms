6e89d4229f7ff4ec994d487de490bb7427b407e8
lihui <fastso.biko@gmail.com>
2019-03-19 16:49:34 +0900

    commit

---
+++ src/leetcode/problems/v258/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.problems.v258;
+
+public class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num >= 10) {
+                sum += num % 10;
+                num /= 10;
+            }
+            sum += num;
+            num = sum;
+        }
+        return num;
+    }
+}
