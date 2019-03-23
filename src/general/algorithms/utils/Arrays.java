4f5509bdabd7913e16de94441b51b82f42bd659d
lihui <fastso.biko@gmail.com>
2019-03-23 19:24:30 +0900

    commit

--- src/general/algorithms/utils/Arrays.java
+++ src/general/algorithms/utils/Arrays.java
@@ -64,4 +64,18 @@ public class Arrays {
         }
         return false;
     }
+
+    /**
+     * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+     *
+     * @param nums 配列
+     * @return 一つしか存在しない数
+     */
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i=0;i<nums.length;i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
 }
