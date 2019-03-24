42ecf6006c87f9714ee9f043894bc9f3ba4a5697
lihui <fastso.biko@gmail.com>
2019-03-24 13:53:49 +0900

    commit

--- src/general/algorithms/utils/Arrays.java
+++ src/general/algorithms/utils/Arrays.java
@@ -47,6 +47,21 @@ public class Arrays {
         }
     }
 
+    public void reverse(char[] s) {
+        reverse(s, 0, s.length-1);
+    }
+
+    public void reverse(char[] s, int start, int end) {
+        while (start < end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+
     /**
      * 重複データ有無を判定する。
      *
