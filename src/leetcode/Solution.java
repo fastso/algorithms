2b1a6c9476b7a0984f3218d735747027ce312b3c
lihui <fastso.biko@gmail.com>
2019-03-28 11:35:36 +0900

    commit

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -5,23 +5,48 @@ import java.util.*;
 
 public class Solution {
     public static void main(String... args) {
+        Solution sol = new Solution();
 
+//        int[] A = {2,1,2,5,3,2};
+//        System.out.println(sol.repeatedNTimes(A));
+
+        System.out.println(sol.judgeCircle("LDRRLRUULR"));
     }
 
-    public int[] sortArrayByParity(int[] A) {
-        int[] result = new int[A.length];
+    public boolean judgeCircle(String moves) {
+        int rl = 0;
+        int ud = 0;
 
-        int i1 = 0;
-        int i2 = A.length-1;
-        for (int i = 0; i < A.length; i++) {
-            if (A[i]%2 == 0) {
-                result[i1] = A[i];
-                i1++;
-            } else {
-                result[i2] = A[i];
-                i2--;
+        char[] cMoves = moves.toCharArray();
+        for (char c : cMoves) {
+            switch (c) {
+                case 'U' :
+                    ud++;
+                    break;
+                case 'D' :
+                    ud--;
+                    break;
+                case 'L' :
+                    rl++;
+                    break;
+                case 'R' :
+                    rl--;
+                    break;
             }
         }
-        return result;
+        if (rl == 0 && ud == 0) return true;
+        else return false;
+    }
+
+    public int repeatedNTimes(int[] A) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x : A) {
+            map.put(x, map.getOrDefault(x,0)+1);
+        }
+
+        for (int k:map.keySet()) {
+            if (map.get(k) > 1) return k;
+        }
+        return 0;
     }
 }
