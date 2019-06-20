8087905c9a1a9e6813d1e255371ea7a311fcff43
lihui <fastso.biko@gmail.com>
2019-06-20 14:56:26 +0900

    commit

---
+++ src/aoj/course/itp1/itp1_n_a/Main.java
@@ -0,0 +1,11 @@
+package aoj.course.itp1.itp1_n_a;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String... args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println();
+    }
+}
