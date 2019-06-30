d9a6dcae619e3b7243e0dafab62d76c5af912402
lihui <fastso.biko@gmail.com>
2019-06-30 22:46:17 +0900

    commit

--- src/atcoder/contest/abc132/d/Main.java
+++ src/atcoder/contest/abc132/d/Main.java
@@ -1,31 +1,43 @@
 package atcoder.contest.abc132.d;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
+    static final BigInteger MOD = BigInteger.valueOf(1000000007);
+
     public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        long[] a = new long[k + 1];
+        BigInteger[] a = new BigInteger[k + 1];
 
         for (int i = 1; i <= k; i++) {
-            if (i == 1) {
-                a[i] = nCr(n-k+1, i)%1000000007;
-            } else {
-                a[i] = (nCr(n-k+1, i)%1000000007) * (nCr(k-1,i-1)%1000000007);
-                a[i] %= 1000000007;
+            if (n - k - i + 1 < 0) {
+                System.out.println(0);
+                continue;
             }
+            a[i] = nCr(n - k + 1, i).multiply(nCr(k - 1, i - 1)).mod(MOD);
             System.out.println(a[i]);
         }
-
     }
 
-    public static long nCr(int n, int r) {
-        long num = 1;
-        for (int i = 1; i <= r; i++) {
-            num = num * (n - i + 1) / i % 1000000007;
+    private static BigInteger nCr(int a, int b) {
+        BigInteger ret = BigInteger.ONE;
+        for (int i=a; i > a-b; i--) {
+            ret = ret.multiply(BigInteger.valueOf(i));
         }
-        return num;
+        for (int i=1; i <= b; i++) {
+            ret = ret.divide(BigInteger.valueOf(i));
+        }
+        return ret;
     }
+
+//    public static BigInteger nCr(int n, int r) {
+//        BigInteger num = BigInteger.ONE;
+//        for (int i = 1; i <= r; i++) {
+//            num = num.multiply(BigInteger.valueOf(n - i + 1)).divide(BigInteger.valueOf(i));
+//        }
+//        return num;
+//    }
 }
